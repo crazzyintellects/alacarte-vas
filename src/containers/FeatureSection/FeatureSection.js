f1973c849761d7890dde5dc6de2c920c7204cf9f
Neha Kakkar <sushantsaini1@gmail.com>
2018-08-14 13:56:03 -0600

    AcceptButton

--- src/containers/FeatureSection/FeatureSection.js
+++ src/containers/FeatureSection/FeatureSection.js
@@ -1,33 +1,102 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import blue from "@material-ui/core/colors/blue";
+import green from "@material-ui/core/colors/green";
 import Button from "@material-ui/core/Button";
+import CheckIcon from "@material-ui/icons/Check";
+import SaveIcon from "@material-ui/icons/Save";
 
 const styles = theme => ({
-  button: {
+  root: {
+    display:"flex",
+    alignItems:"center",
+    marginTop:"10%",
+    marginLeft:"45%"
+  },
+  wrapper: {
     margin: theme.spacing.unit,
-    marginTop: "30%",
-    marginLeft: "50%"
+    position: "relative"
+  },
+  buttonSuccess: {
+    backgroundColor: blue[500],
+    "&:hover": {
+      backgroundColor: blue[700]
+    }
+  },
+  buttonProgress: {
+    color: blue[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12
   }
 });
-/*
-openFeatures = () => {
-  location.assign('../../features.html');
-};
-*/
-function ContainedButtons(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <Button variant="contained" /*onClick={this.openFeatures()}*/ color="primary" className={classes.button}>
-        Accept
-      </Button>
-    </div>
-  );
+
+class CircularIntegration extends React.Component {
+  timer = null;
+
+  state = {
+    loading: false,
+    success: false
+  };
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  handleButtonClick = () => {
+    if (!this.state.loading) {
+      this.setState(
+        {
+          success: false,
+          loading: true
+        },
+        () => {
+          this.timer = setTimeout(() => {
+            this.setState({
+              loading: false,
+              success: true
+            });
+          }, 2000);
+        }
+      );
+    }
+  };
+
+  render() {
+    const { loading, success } = this.state;
+    const { classes } = this.props;
+    const buttonClassname = classNames({
+      [classes.buttonSuccess]: success
+    });
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.wrapper}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={buttonClassname}
+            disabled={loading}
+            onClick={this.handleButtonClick}
+          >
+            Accept terms
+          </Button>
+          {loading && (
+            <CircularProgress size={24} className={classes.buttonProgress} />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-ContainedButtons.propTypes = {
+CircularIntegration.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(ContainedButtons);
+export default withStyles(styles)(CircularIntegration);
